640e96917aaf102832ba45abc08412084c383ad9
Alperen Bakirci <bakirci.alperen@gmail.com>
2023-11-06 16:17:46 -0600

    Added active section for navigation with a little bit of animation

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 import { links } from '@/lib/data';
 import Link from 'next/link';
+import clsx from 'clsx';
 
 export default function Header() {
-
+  const [activeSection, setActiveSection] = useState('Home');
+  
   return (
     <header className="z-[999] relative">
         <motion.div className='fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full
@@ -21,15 +23,31 @@ export default function Header() {
           <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5'>
             {links.map(link => (
               <motion.li 
-              className="h-3/4 flex item-center justify-center" 
+              className="h-3/4 flex item-center justify-center relative" 
               key={link.hash}
               initial={{ opacity: 0 , y: -100 }}
               animate={{ opacity: 1 , y: 0 }}
               >
-                <Link className='flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition' href={link.hash}>
-                {
-                  link.name
-                }
+                <Link 
+                  className={clsx('flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition'
+                    , { 
+                  'text-gray-950': activeSection === link.name
+                  })} 
+                  href={link.hash}
+                  onClick={() => setActiveSection(link.name)}
+                >
+                {link.name}
+
+                {link.name === activeSection && (
+                  <motion.span className='bg-gray-100 rounded-full absolute inset-0 -z-10'
+                  layoutId='activeSection'
+                  transition={{
+                    type: 'spring',
+                    stiffness: 380,
+                    damping: 30
+                  }}
+                  ></motion.span>
+                  )}
                 </Link>
               </motion.li>
             ))
